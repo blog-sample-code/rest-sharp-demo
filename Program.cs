4b0cdcc2ae96628582f54ff5f8ca2cb02a4bb249
santoshkaranam <karanam@iiasa.ac.at>
2023-12-27 07:26:42 +0100

    IronPdf

--- Program.cs
+++ Program.cs
@@ -9,6 +9,73 @@ class Program
     static void Main()
     {
         // Create a RestClient instance
+        var baseUrl = "https://jsonplaceholder.typicode.com/users";
+        RestClientOptions options = new RestClientOptions(baseUrl) { UseDefaultCredentials = true };
+        var client = new RestClient(options);
+
+        // Create a RestRequest for the GET method
+        var request = new RestRequest();
+
+        // Execute the request and get the response
+        var response = client.Get(request);
+
+        // Check if the request was successful
+        if (response.IsSuccessful)
+        {
+            // Deserialize JSON response into a list of User objects
+            var users = JsonSerializer.Deserialize<List<User>>(response.Content);
+
+            // Generate Pdf
+            var html = GetHtml(users);
+            var Renderer = new ChromePdfRenderer();
+            var PDF = Renderer.RenderHtmlAsPdf(html);
+            PDF.SaveAs("UsersReport.pdf");
+        }
+        else
+        {
+            // Handle the error
+            Console.WriteLine($"Error: {response.ErrorMessage}");
+        }
+    }
+
+    private static string GetHtml(List<User>? users)
+    {
+        string header = $@"
+<html>
+<head><title>Users List</title></head>
+<body>
+    ";
+        var footer = @"
+</body>
+</html>";
+
+        var htmlContent = header;
+
+        foreach (var user in users)
+        {
+            htmlContent += $@"
+    <h1>{user.Name}</h1>
+    <p>Username: {user.Username}</p>
+    <p>Email: {user.Email}</p>
+    <p>Company: {user.Company}</p>
+    <p>Phone: {user.Phone}</p>
+    <p>Website: {user.Website}</p>
+    
+    <p>Suite: {user.Address.Suite}</p>
+    <p>Street: {user.Address.Street}</p>
+    <p>City: {user.Address.City}</p>
+    <p>Zipcode: {user.Address.Zipcode}</p>
+";
+        }
+
+        htmlContent += footer;
+
+        return htmlContent;
+    }
+
+    private static void GenerateZip()
+    {
+        // Create a RestClient instance
         var baseUrl = "https://github.com/santoshkaranam/rest-sharp-demo/raw/main/images.zip";
         RestClientOptions options = new RestClientOptions(baseUrl) { };
         var client = new RestClient(options);
@@ -26,9 +93,9 @@ class Program
         // Execute the request and get the response
         byte[]? response = client.DownloadData(request);
         if (response != null) File.WriteAllBytes(file, response);
-        
+
         //unzip
-        IronZip.IronZipArchive.ExtractArchiveToDirectory(file,"output");
+        IronZip.IronZipArchive.ExtractArchiveToDirectory(file, "output");
     }
 
     private static void PostMethodDemo()
